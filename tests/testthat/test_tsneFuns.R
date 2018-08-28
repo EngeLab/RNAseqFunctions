2abd852f9312cc02f3bc4ec42ae6f612a4e87fe2
jasonserviss <jason.serviss@ki.se>
2018-08-28 12:03:10 +0200

    5 working on input checks and unit testing

---
+++ tests/testthat/test_tsneFuns.R
@@ -0,0 +1,37 @@
+context("tsneFuns")
+
+test_that("check pearsonsCor with expected input", {
+
+  #setup normal input data
+  mat <- matrix(rep(100:110, 10), ncol = 10)
+
+  #setup expected data
+  expected <- as.dist(matrix(rep(0, 100), ncol = 10))
+  attr(expected, "call") <- NULL
+
+  #run function
+  output <- pearsonsCor(mat)
+  attr(output, "call") <- NULL
+
+  #test
+  expect_equal(expected, output)
+
+  #setup normal input data
+  mat <- matrix(rep(100:110, 10), ncol = 10)
+
+  #setup expected data
+  expected <- as.dist(matrix(rep(2.220446e-16, 100), ncol = 10))
+  attr(expected, "call") <- NULL
+
+  #run function
+  output <- pearsonsCor(mat, 1:10)
+  attr(output, "call") <- NULL
+
+  #test
+  expect_equal(expected, output)
+})
+
+test_that("check runTsne with expected input", {
+  pc <- pearsonsCor(testingCounts[, -1])
+  expect_silent(runTsne(pc, perplexity = 2))
+})
