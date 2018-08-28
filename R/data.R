f3e1fb95b11d6682ebe7ae8f64f77ab193150ded
jasonserviss <jason.serviss@ki.se>
2018-08-28 17:21:07 +0200

    adding processed dataset for additional vignettes to be added

---
+++ R/data.R
@@ -0,0 +1,67 @@
+#' Test counts
+#'
+#' A small example data frame used for demonstrating and testing the counts data
+#' processing functions available in the package.
+#'
+#' @title Simulated data used for testing and demos.
+#' @docType data
+#' @name testingCounts
+#' @format Matrix counts, with:
+#' \describe{
+#' \item{rownames}{Gene names}
+#' \item{colnames}{Samples/cells}
+#' }
+#' @keywords datasets
+#' @examples
+#' data(testingCounts)
+"testingCounts"
+
+#' Data for testing metadata processing functions.
+#'
+#' @title Data for testing metadata processing functions.
+#' @docType data
+#' @name testingMeta
+#' @format Tibble with:
+#' \describe{
+#' \item{sample}{Sample name}
+#' \item{row}{numeric; indicates the plate row number}
+#' \item{column}{numeric; indicates the plate column number}
+#' }
+#' @keywords datasets
+#' @examples
+#' data(testingMeta)
+#'
+"testingMeta"
+
+#' counts
+#'
+#' A small example dataset of procesed counts.
+#'
+#' @title scRNAseq data from 3 cell lines.
+#' @docType data
+#' @name counts
+#' @format Matrix counts, with:
+#' \describe{
+#' \item{rownames}{Gene names}
+#' \item{colnames}{Samples/cells}
+#' }
+#' @keywords datasets
+#' @examples
+#' data(counts)
+"counts"
+
+#' Metadata for the counts dataset.
+#'
+#' @title Metadata for the counts dataset.
+#' @docType data
+#' @name testingMeta
+#' @format Tibble with:
+#' \describe{
+#' \item{sample}{Sample name}
+#' \item{cellTypes}{character; indicates the cell type}
+#' }
+#' @keywords datasets
+#' @examples
+#' data(meta)
+#'
+"meta"
---
+++ inst/rawdata/processedCounts.R
@@ -0,0 +1,21 @@
+#requires sp.scRNAseqData package
+#run from package root with: source('inst/rawdata/processedCounts.R')
+
+#counts
+packages <- c("sp.scRNAseqData", "tidyverse")
+purrr::walk(packages, library, character.only = TRUE)
+rm(packages)
+
+lg <- grepl("^s", colnames(countsSorted2))
+rv <- matrixStats::rowMaxs(countsSorted2[, lg])
+select <- order(rv, decreasing = TRUE)[1:2000]
+
+counts <- rbind(countsSortedERCC2[, lg], countsSorted2[select, lg]) %>%
+  .[, rev(order(colnames(.)))]
+
+#meta
+meta <- countsSortedMeta2 %>%
+filter(sample %in% colnames(countsSorted2)[lg]) %>%
+select(sample, cellTypes)
+
+save(counts, meta, file = "data/processedCounts.rda", compress = "bzip2")
